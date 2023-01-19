f658b84f94e113e2a410f586c3fee2835695ffad
6-ON <aminhatim6@gmail.com>
2023-01-19 20:43:50 +0100

    Create Product and User Model

---
+++ models/Product.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace CureCo\models;
+
+use sixon\hwFramework\Application;
+use sixon\hwFramework\db\DbModel;
+
+class Product extends DbModel
+{
+    public $name;
+    public $price;
+    public $quantity;
+    public $image;
+
+    public static function tableName(): string
+    {
+        return 'products';
+    }
+
+    public function attributes(): array
+    {
+        return ['name', 'price', 'quantity', 'image'];
+    }
+
+    public static function primaryKey(): string
+    {
+        return 'id';
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => [self::RULE_REQUIRED],
+            'price' => [self::RULE_REQUIRED],
+            'quantity' => [self::RULE_REQUIRED],
+            'image' => [self::RULE_REQUIRED]
+        ];
+
+    }
+
+    public static function search(string $term): false|array
+    {
+        $stmt = Application::$app->db->pdo->prepare("SELECT * FROM products WHERE name LIKE CONCAT('%',:term,'%')");
+        $stmt->bindValue(':term',$term);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_CLASS,self::class);
+
+
+    }
+
+    public function labels(): array
+    {
+        return [];
+    }
+}
---
+++ models/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace CureCo\models;
+
+use sixon\hwFramework\UserModel;
+
+class User extends UserModel
+{
+    public $username;
+    public $email;
+    public $password;
+
+    public $image;
+
+    public static function tableName(): string
+    {
+        return 'users';
+    }
+
+    public function attributes(): array
+    {
+        return ['email', 'password', 'username'];
+    }
+
+    public static function primaryKey(): string
+    {
+        return 'id';
+    }
+
+    public function save()
+    {
+        $this->password = password_hash($this->password, PASSWORD_DEFAULT);
+        return parent::save();
+    }
+
+    public function rules(): array
+    {
+        return [
+            'email' => [self::RULE_REQUIRED],
+            'username' => [self::RULE_REQUIRED],
+            'password' => [self::RULE_REQUIRED]
+        ];
+    }
+
+    public function labels(): array
+    {
+        return [];
+    }
+
+    public function getDisplayName(): string
+    {
+        return $this->username;
+    }
+
+    public function getDisplayEmail(): string
+    {
+        return $this->email;
+    }
+}
