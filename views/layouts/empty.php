c119582e2c968a947f93a0af291466f03466bdcf
6-ON <aminhatim6@gmail.com>
2023-01-19 20:39:37 +0100

    Create empty layout

---
+++ views/layouts/empty.php
@@ -0,0 +1,19 @@
+<?php
+/* @var $this \sixon\hwFramework\View */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $this->title ?></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="css/output.css">
+
+</head>
+<body>
+{{content}}
+</body>
+</html>
