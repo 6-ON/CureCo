9ab07543d20bab4d1537e3cc53061239591bcb15
6-ON <aminhatim6@gmail.com>
2023-01-22 17:48:07 +0100

    add dark theme to body

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,5 +1,8 @@
 <?php
 /* @var $this \sixon\hwFramework\View */
+
+use sixon\hwFramework\Application;
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -15,7 +18,7 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.0"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
-<body>
+<body class="dark:bg-gray-800">
 <header class="sticky top-0 z-30 shadow-sm">
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div class="flex flex-wrap justify-between items-center">
@@ -42,17 +45,17 @@
                 <!-- Profile     -->
                 <button type="button" class="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="dropdown">
                     <span class="sr-only">Open user menu</span>
-                    <img class="w-8 h-8 rounded-full" src="/img/users/usr-1.jpg" alt="user photo">
+                    <img class="w-8 h-8 rounded-full" src="<?php echo Application::$app->user->getDisplayImage() ?>" alt="user photo">
                 </button>
                 <!-- Dropdown menu -->
                 <div class="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown">
                     <div class="py-3 px-4">
-                        <span class="block text-sm font-semibold text-gray-900 dark:text-white">Neil sims</span>
-                        <span class="block text-sm font-light text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                        <span class="block text-sm font-semibold text-gray-900 dark:text-white"><?= Application::$app->user->getDisplayName() ?></span>
+                        <span class="block text-sm font-light text-gray-500 truncate dark:text-gray-400"><?= Application::$app->user->getDisplayEmail() ?></span>
                     </div>
                     <ul class="py-1 font-light text-gray-500 dark:text-gray-400" aria-labelledby="dropdown">
                         <li>
-                            <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+                            <a href="/logout" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
                         </li>
                     </ul>
                 </div>
