070b6ac6eb8ed2b3e1bbb89ec551f8d35af1ec28
6-ON <aminhatim6@gmail.com>
2023-01-23 17:51:41 +0100

    add ordering functionality

--- models/Product.php
+++ models/Product.php
@@ -37,12 +37,19 @@ class Product extends DbModel
 
     }
 
-    public static function search(string $term): false|array
+    public static function search(string $term='', $orderBy = []): false|array
     {
-        $stmt = Application::$app->db->pdo->prepare("SELECT * FROM products WHERE name LIKE CONCAT('%',:term,'%')");
-        $stmt->bindValue(':term',$term);
+        $sql = "SELECT * FROM products WHERE name LIKE CONCAT('%',:term,'%')";
+        if (!empty($orderBy)) {
+            $sql .= ' ORDER BY ' . implode(',', array_map(function ($v, $k) {
+                    return sprintf("%s %s", $k, $v);
+                },$orderBy,array_keys($orderBy)));
+        }
+
+        $stmt = Application::$app->db->pdo->prepare($sql);
+        $stmt->bindValue(':term', $term);
         $stmt->execute();
-        return $stmt->fetchAll(\PDO::FETCH_CLASS,self::class);
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, self::class);
 
 
     }
